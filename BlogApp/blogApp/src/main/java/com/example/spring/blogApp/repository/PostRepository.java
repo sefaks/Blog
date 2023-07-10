ab758ec536ec3651bc191d050d5199c7d098cac3
Yusuf Sefa <sefakse11@gmail.com>
2023-07-10 23:33:17 +0300

    trying

--- BlogApp/blogApp/src/main/java/com/example/spring/blogApp/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.spring.blogApp.repository;
-
-import com.example.spring.blogApp.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post,Long> {
-
-}
